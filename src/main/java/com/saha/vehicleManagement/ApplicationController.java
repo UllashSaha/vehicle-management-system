0860e2960d95d3f554e13d8039de86d112fcf8f4
sahaullash <ullashsaha12@gmail.com>
2021-01-06 19:10:35 -0600

    login-v1

--- src/main/java/com/saha/vehicleManagement/ApplicationController.java
+++ src/main/java/com/saha/vehicleManagement/ApplicationController.java
@@ -11,4 +11,12 @@ public class ApplicationController {
     	return "index";   //it will return the index.html page
     }
 	
+	@GetMapping("/login") // if I get /index     
+    public String login() {
+    	return "login";   //it will return the index.html page
+    }
+	@GetMapping("/logout") // if I get /index     
+    public String logout() {
+    	return "login";   //it will return the index.html page
+    }
 }
--- src/main/java/com/saha/vehicleManagement/repositories/UserRepository.java
+++ src/main/java/com/saha/vehicleManagement/repositories/UserRepository.java
@@ -7,5 +7,7 @@ import com.saha.vehicleManagement.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+	
+	User findByUsername(String username);
 
 }
